06e0eb85139060076cfc5e847875db51a7135f89
harshitpurwar <hpurwar@gmail.com>
2021-01-26 20:44:56 +0530

    Updating home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,48 +1,100 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
+import styles from '../templates/Bloglist.module.scss';
 
 
-import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { rhythm } from "../utils/typography"
 
-const BlogIndex = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata.title
-  const posts = data.allMarkdownRemark.edges
+
+//const BlogIndex = ({ data, location }) => {
+class BlogIndex extends React.Component {
+  render() {
+    const { data } = this.props
+    const siteTitle = data.site.siteMetadata.title
+    const posts = data.allMarkdownRemark.edges
+    const { currentPage, numPages } = this.props.pageContext
+    const isFirst = currentPage === 1
+    const isLast = currentPage === numPages
+    const prevPage = currentPage - 1 === 1 ? '/' : (currentPage - 1).toString()
+    const nextPage = (currentPage + 1).toString()
 
   return (
-    <Layout location={location} title={siteTitle}>
-      <SEO title="All posts" />
+    <Layout location={this.props.location} title={siteTitle}>
+      <SEO
+        title={siteTitle}
+        keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+      />
 
+      <div className="grid grid-cols-2 gap-10 mb-8 p-4">
       {posts.map(({ node }) => {
         const title = node.frontmatter.title || node.fields.slug
+        const titleC = node.frontmatter.titleC
         return (
-          <article className="card rounded-lg p-5" key={node.fields.slug}>
-            <header>
-              <h3
-                style={{
-                  marginBottom: rhythm(1 / 4),
-                }}
-              >
-                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <small>{node.frontmatter.date}</small>
+
+          <article key={node.fields.slug} className="card">
+          <header className="relative">
+            <h2 className={`heading-two  ${styles.blog_list_heading}`}>
+              <Link to={node.fields.slug}>
+                {title}
+              </Link>
+            </h2>
+            <div className="text-gray mb-2 mt-1 font-1 block">Published on: {node.frontmatter.date} / {node.timeToRead} min read</div>
+            <div aria-hidden="true" className={`absolute ${styles.post_title}`}>{titleC}</div>
             </header>
-            <section>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: node.frontmatter.description || node.excerpt,
-                }}
-              />
-            </section>
+            <p className="mb-5" dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+            <Link to={node.fields.slug} className={`hvr-sweep-to-right mb-2 inline-block ${styles.more_button}`}>
+              Continue Reading <span className='font-2 ml-0.5' aria-hidden="true">&#187;</span>
+            </Link>
           </article>
         )
       })}
+      </div>
+      <nav aria-label="pagination" className="flex justify-center">
+      <ul className="pagination flex"
+        // style={{
+        //   display: 'flex',
+        //   flexWrap: 'wrap',
+        //   justifyContent: 'center',
+        //   alignItems: 'center',
+        //   listStyle: 'none',
+        //   padding: 0,
+        // }}
+      >
+        {!isFirst && (
+          <Link className="" to={`../${prevPage}`} rel="prev">
+            ← Previous Page
+          </Link>
+        )}
+        {Array.from({ length: numPages }, (_, i) => (
+          <li className={`${styles.pagination_list__item}`}
+            key={`pagination-number${i + 1}`}
+
+          >
+            <Link className="pagination-link"
+              to={`/${i === 0 ? '' : i + 1}`}
+              style={{
+
+                textDecoration: 'none',
+                color: i + 1 === currentPage ? '#ffffff' : '',
+                background: i + 1 === currentPage ? '#007acc' : '',
+                borderColor: i + 1 === currentPage ? '#007acc' : '',
+              }}
+            >
+              {i + 1}
+            </Link>
+          </li>
+        ))}
+        {!isLast && (
+          <Link className="" to={`../${nextPage}`} rel="next">
+            Next Page →
+          </Link>
+        )}
+      </ul>
+      </nav>
     </Layout>
   )
+}
 }
 
 export default BlogIndex
@@ -65,6 +117,7 @@ export const pageQuery = graphql`
             date(formatString: "MMMM DD, YYYY")
             title
             description
+            titleC
           }
         }
       }
