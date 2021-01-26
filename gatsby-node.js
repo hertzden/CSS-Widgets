fe97c44264b145cef5844e49df8dbbb0612d7fb1
harshitpurwar <hpurwar@gmail.com>
2021-01-26 20:23:28 +0530

    updating gatsby node file

--- gatsby-node.js
+++ gatsby-node.js
@@ -57,7 +57,7 @@ exports.createPages = ({ graphql, actions }) => {
     Array.from({ length: numPages }).forEach((_, i) => {
       createPage({
         path: i === 0 ? `/` : `/${i + 1}`,
-        component: path.resolve('./src/templates/blog-list.js'),
+        component: path.resolve('./src/pages/index.js'),
         context: {
           limit: postsPerPage,
           skip: i * postsPerPage,
